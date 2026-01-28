70f36e21b76e806ab93c80c1b315759f9b2eb2e5
andrew-marquezin <andrewmmarinho@gmail.com>
2026-01-28 10:54:37 -0300

    feat: add register form

---
+++ frontend/src/pages/Auth/Register.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import "./Auth.css";
+
+export default function Register() {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="text" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem conta? Faça login <Link to="/login">aqui</Link>
+      </p>
+    </div>
+  );
+}
