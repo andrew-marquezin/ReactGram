ccbe88b22e8f1a4d4a9153d05e23fe4357394070
andrew-marquezin <andrewmmarinho@gmail.com>
2026-01-29 15:25:47 -0300

    feat: add conditional rendering for authenticated user

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -6,8 +6,14 @@ import {
   BsFillPersonFill,
   BsFillCameraFill,
 } from "react-icons/bs";
+import { useAuth } from "../../hooks/useAuth";
+import { useSelector } from "react-redux";
+import type { RootState } from "../../store";
 
 export default function Navbar() {
+  const { auth } = useAuth();
+  const { user } = useSelector((state: RootState) => state.auth);
+
   return (
     <nav id="nav">
       <Link to="/">ReactGram</Link>
@@ -16,17 +22,39 @@ export default function Navbar() {
         <input type="text" placeholder="Pesquisar" />
       </form>
       <ul id="nav-links">
-        <li>
-          <NavLink to="/">
-            <BsHouseDoorFill />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login">Entrar</NavLink>
-        </li>
-        <li>
-          <NavLink to="/register">Cadastrar</NavLink>
-        </li>
+        {auth ? (
+          <>
+            <li>
+              <NavLink to="/">
+                <BsHouseDoorFill />
+              </NavLink>
+            </li>
+            {user && (
+              <li>
+                <NavLink to={`/users/${user._id}`}>
+                  <BsFillCameraFill />
+                </NavLink>
+              </li>
+            )}
+            <li>
+              <NavLink to="/profile">
+                <BsFillPersonFill />
+              </NavLink>
+            </li>
+            <li>
+              <span>Sair</span>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <NavLink to="/login">Entrar</NavLink>
+            </li>
+            <li>
+              <NavLink to="/register">Cadastrar</NavLink>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
