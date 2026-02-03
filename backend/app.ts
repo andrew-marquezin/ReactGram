5de906be437f0f5de52f0064ea39710620a1cb9f
andrew-marquezin <andrewmmarinho@gmail.com>
2026-02-03 09:34:36 -0300

    fix: backend fixes for routing and updating user image

--- backend/app.ts
+++ backend/app.ts
@@ -19,7 +19,7 @@ app.use(
   }),
 );
 
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 require("./config/db");
 
