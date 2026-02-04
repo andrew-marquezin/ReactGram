0a729b3f3ba6dbd660543f71f75500216ad436b0
andrew-marquezin <andrewmmarinho@gmail.com>
2026-02-04 14:16:26 -0300

    feat: photo services and slices

---
+++ frontend/src/services/photoService.ts
@@ -0,0 +1,67 @@
+import { api, requestConfig } from "../utils/config";
+
+const publishPhoto = async (data: FormData, token: string) => {
+  const config = requestConfig("POST", data, token, true);
+
+  try {
+    const res = await fetch(api + "/photos", config)
+      .then((res) => res.json())
+      .catch((err) => err.json());
+
+    return res;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getUserPhotos = async (id: string, token: string) => {
+  const config = requestConfig("GET", null, token);
+
+  try {
+    const res = await fetch(api + "/photos/user/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err.json());
+    return res;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const deletePhoto = async (id: string, token: string) => {
+  const config = requestConfig("DELETE", null, token);
+
+  try {
+    const res = await fetch(api + "/photos/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err.json());
+    return res;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const updatePhoto = async (
+  data: { title: string },
+  id: string,
+  token: string,
+) => {
+  const config = requestConfig("PUT", data, token);
+
+  try {
+    const res = await fetch(api + "/photos/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err.json());
+    return res;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const photoService = {
+  publishPhoto,
+  getUserPhotos,
+  deletePhoto,
+  updatePhoto,
+};
+
+export default photoService;
